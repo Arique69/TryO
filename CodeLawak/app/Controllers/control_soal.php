da24b643d0781844b7cf3baf0dbd689ccc4df4a4
Rizki Amanullah Hakim <rizkiamanullah723@gmail.com>
2021-05-07 10:35:06 +0700

    tambah controller, model punya soal, paket, mapel. perubahan button pada menuguru

---
+++ CodeLawak/app/Controllers/control_soal.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Soal;
+
+class control_soal extends BaseController
+{
+    // variable tampung dari database
+    protected $soal;
+
+    // __construct
+    public function __construct()
+    {
+        $this->soal = new Soal();
+    }
+
+    // halaman awal pada saat kelola soal
+    public function index()
+    {
+        $data['soal'] = $this->soal->ambil_soal();
+        //$data = $guru->ambil_guru();
+        echo view('template/header');
+        echo view('kelolasoal', $data);
+    }
+
+
+    //untuk mengecek view header navbarnya
+    public function header()
+    {
+        return view('template/header');
+    }
+
+    #untuk menampilkan fitur kelola guru
+    public function kelolasoal()
+    {
+        //mengambil session
+        session();
+        //validasi data untuk memenuhi beberapa aturan
+        $data = [
+            'validation' => \Config\Services::validation()
+        ];
+        echo view('template/header');
+        echo view('kelolaguru', $data);
+    }
+
+    public function insert_soal()
+    {
+        //validasi input berupa aturan dan error messagenya
+        if (!$this->validate([
+            'id_mata_pelajaran' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Mata Pelajaran harus diisi.'
+                ]
+            ],
+            'isi_soal' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Isi soal harus diisi.'
+                ]
+            ],
+            'opsi_a' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Opsi A harus diisi.'
+                ]
+            ],
+            'opsi_b' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Opsi B harus diisi.'
+                ]
+            ],
+            'opsi_c' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Opsi C harus diisi.'
+                ]
+            ],
+            'opsi_d' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Opsi D harus diisi.'
+                ]
+            ],
+            'kunci_jawaban' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Kunci Jawaban harus diisi.'
+                ]
+            ]
+        ])) {
+            //mengirimkan validasi ke view
+            $validation = \Config\Services::validation();
+            return redirect()->to(base_url('control_soal/kelolasoal'))->withInput()->with('validation', $validation);
+        }
+
+        //mengambil data dari view
+        $isi_soal = $this->request->getPost('isi_soal');
+        $opsi_a = $this->request->getPost('opsi_a');
+        $opsi_b = $this->request->getPost('opsi_b');
+        $opsi_c = $this->request->getPost('opsi_c');
+        $opsi_d = $this->request->getPost('opsi_d');
+        $kunci_jawaban = $this->request->getPost('kunci_jawaban');
+
+        //memasukan ke array data
+        $data = [
+            'isi_soal' => $isi_soal,
+            'opsi_a' => $opsi_a,
+            'opsi_b' => $opsi_b,
+            'opsi_c' => $opsi_c,
+            'opsi_d' => $opsi_d,
+            'kunci_jawaban' => $kunci_jawaban
+        ];
+
+        //melakukan proses input data soal baru ke database 'soal' 
+        $this->soal->input_soal($data);
+        //jika ada flash data maka tampilkan
+        session()->setFlashdata('pesan', 'data berhasil ditambahkan');
+        //redirect ke view main menu soal
+        return redirect()->to(base_url('control_soal'));
+    }
+}
--- CodeLawak/app/Views/menuguru.php
+++ CodeLawak/app/Views/menuguru.php
@@ -12,10 +12,12 @@
 </head>
 
 <body>
+    <!-- container dengan informasi bahwa user sedang berada di menu guru -->
     <div class="container" style="margin-top:200px" id="form">
         <h3 class="mb-3"><b>Menu Guru</b></h3>
         <div class="row">
             <div class="alert alert-info">
+                <!-- welcome message -->
                 <b>Selamat Datang ! </b> - Guru
             </div>
             <div class="alert alert-warning mb-3">
@@ -25,7 +27,8 @@
                 <div class="card">
                     <div class="card-body text-center">
                         <i class="fas fa-stream fa-10x mb-5"></i>
-                        <button class="btn btn-lg btn-block btn-primary mt-10">Manage Soal</button>
+                        <!-- button menuju fungsi kelolasoal() -->
+                        <a href="<?php echo base_url('control_soal'); ?>" class="btn btn-lg btn-block btn-primary mt-10">Manage Soal</a>
                     </div>
                 </div>
             </div>
@@ -33,7 +36,8 @@
                 <div class="card">
                     <div class="card-body text-center">
                         <i class="fas fa-book-open fa-10x mb-5"></i>
-                        <button class="btn btn-lg btn-block btn-primary mt-10">Manage Paket</button>
+                        <!-- button menuju fungsi kelolapaket() -->
+                        <a href="<?php echo base_url('control_paket'); ?>" class="btn btn-lg btn-block btn-primary mt-10">Manage Paket</a>
                     </div>
                 </div>
             </div>
@@ -41,7 +45,8 @@
                 <div class="card">
                     <div class="card-body text-center">
                         <i class="fas fa-atom fa-10x mb-5"></i>
-                        <button class="btn btn-lg btn-block btn-primary mt-10">Manage Mata Pelajaran</button>
+                        <!-- button menuju fungsi kelolamapel() -->
+                        <a href="<?php echo base_url('control_mapel'); ?>" class="btn btn-lg btn-block btn-primary mt-10">Manage Mata Pelajaran</a>
                     </div>
                 </div>
             </div>
