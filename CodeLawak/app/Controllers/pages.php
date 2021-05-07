ff002996b074ad42a1a0b1858bce8704121e2aaf
Muhammad Rizq Athariq <58097473+Arique69@users.noreply.github.com>
2021-05-07 09:10:34 +0700

    comm pages

--- CodeLawak/app/Controllers/pages.php
+++ CodeLawak/app/Controllers/pages.php
@@ -16,23 +16,25 @@ class pages extends BaseController
         pada function di dalam class Product 
         */
     }
+    //untuk menampilkan view landing page   
     public function index()
     {
         // echo view('template/header');
         echo view('landingpage');
     }
-
+    //untuk menampilkan view lihat nilai  
     public function lihatnilai()
     {
         echo view('template/header');
         echo view('lihatnilai');
     }
-
+    //untuk menampilkan view kelola guru   
     public function kelolaguru()
     {
         echo view('template/header');
         echo view('kelolaguru');
     }
+    //untuk menampilkan view output guru
     public function output_guru()
     {
         $guru = new Guru();
@@ -40,28 +42,33 @@ class pages extends BaseController
         echo view('template/header');
         echo view('Output_guru', compact('data'));
     }
+    //untuk menampilkan view input siswa
     public function input_siswa()
     {
         echo view('template/header');
         echo view('input_siswa');
     }
+    //untuk menampilkan view kerjakan soal
     public function kerjakan_soal()
     {
         # code...
         echo view('template/header');
         echo view('kerjakansoal');
     }
+    //untuk menampilkan view ganti pass
     public function ganti_pass()
     {
         # code...
         echo view('template/header');
         echo view('test');
     }
+    //untuk menampilkan view login
     public function login()
     {
         # code...
         echo view('Login');
     }
+    //untuk menampilkan view input guru
     public function insert_guru()
     {
         # code...
@@ -82,16 +89,19 @@ class pages extends BaseController
         $this->Guru->input_guru($data);
         return redirect()->to(base_url('/pages/Output_guru'));
     }
+    //untuk menampilkan view page siswa
     public function menu_siswa()
     {
         echo view('template/header');
         echo view('menusiswa');
     }
+    //untuk menampilkan view page admin
     public function menu_admin()
     {
         echo view('template/header');
         echo view('menuadmin');
     }
+    //untuk menampilkan view page guru
     public function menu_guru()
     {
         echo view('template/header');
