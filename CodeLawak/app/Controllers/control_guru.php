11d37e24de25b0c33ed33a6d88b1fd121256d642
Muhammad Rizq Athariq <58097473+Arique69@users.noreply.github.com>
2021-05-28 09:36:59 +0700

    .

--- CodeLawak/app/Controllers/control_guru.php
+++ CodeLawak/app/Controllers/control_guru.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\Guru;
 use Config\Services;
 
@@ -11,16 +12,17 @@ class control_guru extends BaseController
     {
         $this->guru = new Guru();
     }
-    
-	public function index()
-	{
+
+    public function index()
+    {
         $data['guru'] = $this->guru->ambil_guru();
         //$data = $guru->ambil_guru();
         echo view('template/header');
-        echo view('Output_guru',$data);
-	}
+        echo view('Output_guru', $data);
+    }
 
-    public function header(){
+    public function header()
+    {
         return view('template/header');
     }
 
@@ -32,54 +34,52 @@ class control_guru extends BaseController
         ];
 
         echo view('template/header');
-        echo view('kelolaguru',$data);
+        echo view('kelolaguru', $data);
     }
-
-
 
     // function yang melakukan CRUD
     public function insert_guru()
     {
         //validasi input
-        if(!$this->validate([
+        if (!$this->validate([
             'nama' => [
-                'rules' =>'required',
-                'errors' =>[
+                'rules' => 'required',
+                'errors' => [
                     'required' => 'nama harus diisi.'
                 ]
             ],
             'email' => [
-                'rules' =>'required',
-                'errors' =>[
+                'rules' => 'required',
+                'errors' => [
                     'required' => 'email harus diisi.'
                 ]
             ],
 
             'nip' => [
-                'rules' =>'required|is_unique[guru.NIP]',
-                'errors' =>[
+                'rules' => 'required|is_unique[guru.NIP]',
+                'errors' => [
                     'required' => 'NIP harus diisi.',
                     'is_unique' => 'NIP telah terdaftar, masukan NIP yang berbeda'
                 ]
             ],
 
             'username' => [
-                'rules' =>'required|is_unique[guru.username]',
-                'errors' =>[
+                'rules' => 'required|is_unique[guru.username]',
+                'errors' => [
                     'required' => 'Username harus diisi.',
                     'is_unique' => 'Username telah terdaftar, masukan Username yang berbeda'
                 ]
             ],
-            
+
             'password' => [
-                'rules' =>'required|min_length[5]',
-                'errors' =>[
+                'rules' => 'required|min_length[5]',
+                'errors' => [
                     'required' => 'Password harus diisi.',
                     'min_length' => 'masukan minimal 5 karakter Password'
                 ]
             ],
 
-        ])){
+        ])) {
             $validation = \Config\Services::validation();
             return redirect()->to(base_url('control_guru/kelolaguru'))->withInput()->with('validation', $validation);
         }
@@ -101,26 +101,29 @@ class control_guru extends BaseController
         ];
 
         $this->guru->input_guru($data);
-        session()->setFlashdata('pesan','data berhasil ditambahkan');
+        session()->setFlashdata('pesan', 'data berhasil ditambahkan');
         return redirect()->to(base_url('control_guru'));
     }
-    public function delete_guru($id){
+    public function delete_guru($id)
+    {
         $this->guru->deleteguru($id);
-        session()->setFlashdata('pesan','data berhasil dihapus');
+        session()->setFlashdata('pesan', 'data berhasil dihapus');
         return redirect()->to(base_url('control_guru'));
     }
 
-    public function update_guru($id){
+    public function update_guru($id)
+    {
         // $data['guru'] = $this->guru->where(['id_guru', $id]);
         // dd($data);
         $data['guru'] = $this->guru->getGuru($id);
         // $data['guru'] = $this->guru->getGuru($id);
         // $data['guru'] = $this->guru->where('id_guru', $id);
         echo view('template/header');
-        echo view('update_guru',$data);
+        echo view('update_guru', $data);
     }
 
-    public function updateguru(){
+    public function updateguru()
+    {
         $id =  $this->request->getPost('id_guru');
         $nama = $this->request->getPost('nama');
         $email = $this->request->getPost('email');
@@ -137,9 +140,8 @@ class control_guru extends BaseController
             'status_login' => 0
         ];
 
-        $this->guru->updateguruyaha($data,$id);
-        session()->setFlashdata('pesan','data berhasil diubah');
+        $this->guru->updateguruyaha($data, $id);
+        session()->setFlashdata('pesan', 'data berhasil diubah');
         return redirect()->to(base_url('control_guru'));
     }
-
 }
--- CodeLawak/app/Views/template/header.php
+++ CodeLawak/app/Views/template/header.php
@@ -1,70 +1,62 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Sanber - Latihan Bootstrap</title>
-    <!-- <linkrel="stylesheethref="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- <linkrel="stylesheethref="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
       crossorigin="anonymous"
     />" -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">  
-      
-    
-    <!-- <link rel="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css"> -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-    <title>TryO - Tryout berbasis online</title>
-    <style>
-        body {
-            height: 100%;
-            font-size: 1rem;
-            font-family: 'Montserrat', sans-serif;
-        }
 
-        #gradient {
-            background: #536976;
-            /* fallback for old browsers */
-            background: -webkit-linear-gradient(to top, #292E49, #536976);
-            /* Chrome 10-25, Safari 5.1-6 */
-            background: linear-gradient(to top, #292E49, #536976);
-            /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-        }
+  <!-- <link rel="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css"> -->
 
-        a {
-            text-decoration: none;
-            color: black;
-        }
+  <title>TryO - Tryout berbasis online</title>
+  <style>
+    body {
+      height: 100%;
+      font-size: 1rem;
+      font-family: 'Montserrat', sans-serif;
+    }
 
-        html {
-            scroll-behavior: smooth;
-        }
+    #gradient {
+      background: #536976;
+      /* fallback for old browsers */
+      background: -webkit-linear-gradient(to top, #292E49, #536976);
+      /* Chrome 10-25, Safari 5.1-6 */
+      background: linear-gradient(to top, #292E49, #536976);
+      /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    }
 
-    </style>
+    a {
+      text-decoration: none;
+      color: black;
+    }
+
+    html {
+      scroll-behavior: smooth;
+    }
+  </style>
 </head>
 
 <body>
-    <!--Navbar top-->
-    <div class="d-flex justify-content-end bg-dark">
-      <ul class="nav nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#" style="color: white">Help</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#" style="color: white">Contact Admin</a>
-        </li>
-      </ul>
-    </div>
+  <!--Navbar top-->
+  <div class="d-flex justify-content-end bg-dark">
+    <ul class="nav nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color: white">Help</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color: white">Contact Admin</a>
+      </li>
+    </ul>
+  </div>
 
-    <!--Navbar Middle-->
-    <div
-      class="navbar navbar-expand-sm navbar-light justify-content-between"
-      style="background-color: #98bbd3" id="gradient"
-    >
-      <a class="navbar-brand" href="#"
-        ><img src=<?= base_url('img/tryoA.png') ?> alt="" style="width: 150px; margin-left:50px"
-      /></a>
-      <h5 style="color: white; margin-right:50px">Username</h5>
-    </div>
+  <!--Navbar Middle-->
+  <div class="navbar navbar-expand-sm navbar-light justify-content-between" style="background-color: #98bbd3" id="gradient">
+    <a class="navbar-brand" href="#"><img src=<?= base_url('img/tryoA.png') ?> alt="" style="width: 150px; margin-left:50px" /></a>
+    <h5 style="color: white; margin-right:50px"><?php echo session()->get('username'); ?></h5>
+  </div>
